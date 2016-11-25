44c3f12db13487e75b77029dc92f688d8d54c893
dlsrb6342 <dlsrb6342@gmail.com>
2016-11-26 03:54:35 +0900

    폴더 정리

--- sql.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$sign_sql = "INSERT INTO Accounts (ID, PW, email, name, phonenum, zipcode, address1, address2, type)
-			VALUES ('$ID', SHA1('$PW'), '$email', '$name', '$phonenum', '$zipcode', '$address1', '$address2', '$type')";
-	$login_sql = "SELECT * FROM Accounts WHERE ID = '$ID' AND PW = SHA1('$PW')";
-	$productlist_sql = "SELECT Products.productid, Products.productname, Products.price, Products.imageurl, Benchmarks.freezingval
-						FROM Products INNER JOIN Benchmarks ON Products.productid = Benchmarks.productid
-						ORDER BY Products.productid"
-?>
