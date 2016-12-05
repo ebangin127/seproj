26b5cad9dd002403bb1233b56d702665d2c8b61b
dlsrb6342 <dlsrb6342@gmail.com>
2016-12-05 15:35:03 +0900

    파일 정리, 바로 구매

--- js/myCart.js
+++ js/myCart.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
 				temp = data.split("?");
 				if(temp[0] == 0){
 					$("#nothing").html("<br><br><br><br><br>카트가 비어있습니다.<br><br><br><br><br><br><br>");
-					$("#cart").html('<div class="panel-footer text-center"><div class="row"><div class="col-xs-4 continue-shopping"><button type="button" class="btn btn-default btn-block" onclick="$(location).attr(\'href\',\'index.html\')"><strong>계속 쇼핑하기</strong></button></div>')
+					$("#cart").html('<div class="panel-footer text-center"><div class="row"><div class="col-xs-4 continue-shopping"><button type="button" class="btn btn-default btn-block" onclick="$(location).attr(\'href\',\'initialPage.html\')"><strong>계속 쇼핑하기</strong></button></div>')
 				}
 				else{
 					total = parseInt(temp[1]) + parseInt(temp[0]) * 2500;
-					$("#cart").html(temp[2]+'<div class="panel-footer text-center"><div class="row"><div class="col-xs-4 continue-shopping"><button type="button" class="btn btn-default btn-block" onclick="$(location).attr(\'href\',\'index.html\')"><strong>계속 쇼핑하기</strong></button></div><div class="col-xs-5 total"><h4 class="text-center" >총 금액 : <strong>&#8361 '+temp[1]+' + 배송비(<strong>&#8361 </strong>2500 * '+temp[0]+')</strong></h4></div><div class="col-xs-3 payment"><button type="button" class="btn btn-default btn-sm" onclick="$(location).attr(\'href\', \'../htm/payment.html?total='+total+'\')"><span class="glyphicon glyphicon-ok"></span><strong>결제</strong></button></div></div></div>');
+					$("#cart").html(temp[2]+'<div class="panel-footer text-center"><div class="row"><div class="col-xs-4 continue-shopping"><button type="button" class="btn btn-default btn-block" onclick="$(location).attr(\'href\',\'initialPage.html\')"><strong>계속 쇼핑하기</strong></button></div><div class="col-xs-5 total"><h4 class="text-center" >총 금액 : <strong>&#8361 '+temp[1]+' + 배송비(<strong>&#8361 </strong>2500 * '+temp[0]+')</strong></h4></div><div class="col-xs-3 payment"><button type="button" class="btn btn-default btn-sm" onclick="$(location).attr(\'href\', \'../htm/payment.html?total='+total+'\')"><span class="glyphicon glyphicon-ok"></span><strong>결제</strong></button></div></div></div>');
 				}
 			}
 	});
--- php/order_do.php
+++ php/order_do.php
@@ -7,27 +7,37 @@
 	$zipcode = $_POST['zipcode'];
 	$address1 = $_POST['address1'];
 	$address2 = $_POST['address2'];
+	$pid = $_POST['directOrder'];
+	$qty = $_POST['qty'];
+	if($pid == "0"){
+		$cart = sqlMycart($buyer);
+		$count = mysqli_num_rows($cart);
 
-
-	$cart = sqlMycart($buyer);
-	$count = mysqli_num_rows($cart);
-
-	$rowC = mysqli_fetch_assoc($cart);
-	$seller = $rowC['seller'];
-	$orderid = sqlAddorder($seller, $buyer, $name, $phonenum, $zipcode, $address1, $address2);
-	sqlAddop($orderid, $rowC['productid'], $rowC['price'], $rowC['qty']);
-	for($i=1;$i<$count;$i++){
 		$rowC = mysqli_fetch_assoc($cart);
-		if($seller != $rowC['seller']){
-			$seller = $rowC['seller'];
-			$orderid = sqlAddorder($seller, $buyer, $name, $phonenum, $zipcode, $address1, $address2);
-			sqlAddop($orderid, $rowC['productid'], $rowC['price'], $rowC['qty']);
+		$seller = $rowC['seller'];
+		$orderid = sqlAddorder($seller, $buyer, $name, $phonenum, $zipcode, $address1, $address2);
+		sqlAddop($orderid, $rowC['productid'], $rowC['price'], $rowC['qty']);
+		for($i=1;$i<$count;$i++){
+			$rowC = mysqli_fetch_assoc($cart);
+			if($seller != $rowC['seller']){
+				$seller = $rowC['seller'];
+				$orderid = sqlAddorder($seller, $buyer, $name, $phonenum, $zipcode, $address1, $address2);
+				sqlAddop($orderid, $rowC['productid'], $rowC['price'], $rowC['qty']);
+			}
+			else{
+				sqlAddop($orderid, $rowC['productid'], $rowC['price'], $rowC['qty']);
+			}
 		}
-		else{
-			sqlAddop($orderid, $rowC['productid'], $rowC['price'], $rowC['qty']);
-		}
+		sqltrunMycart($buyer);
+		echo "<script>alert('주문이 완료되었습니다.'); location.href = '../htm/completeOrder.html';</script>";
 	}
-	sqltrunMycart($buyer);
-	echo "<script>alert('주문이 완료되었습니다.'); location.href = '../htm/completeOrder.html';</script>";
+	else{
+		$result = sqlProductView($pid);
+		$row = mysqli_fetch_assoc($result);
+		$seller = $row['seller'];
+		$orderid = sqlAddorder($seller, $buyer, $name, $phonenum, $zipcode, $address1, $address2);
+		sqlAddop($orderid, $row['productid'], $row['price'], $qty);
+		echo "<script>alert('바로 구매가 완료되었습니다.'); location.href = '../htm/completeOrder.html';</script>";
+	}
 ?>
 
